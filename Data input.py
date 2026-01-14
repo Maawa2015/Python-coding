0ec21ab36fefc9b5cb030d45f9666453e74130cf
Maawa2015 <mzaman008@gmail.com>
2026-01-14 18:40:19 +0600

    Add files via upload

---
+++ Data input.py
@@ -0,0 +1,26 @@
+a= 20
+print(a, type(a))
+
+b= 5.56
+print(b, type(b))
+
+c= "Hello guys! this is me Afifa Tasnim Nava"
+print(c, type(c))
+
+d= True
+print(d, type(d))
+
+e= [1,3,4,9,6,1]
+print(e, type(e))
+
+f= (2,3,5,6,4)
+print(f,type(f))
+
+g= {1,5,6,8,3}
+print(g, type(g))
+
+h= {"name": "Maawa","age":11 }
+print(h, type(h))
+
+i= None
+print(i, type(i))
