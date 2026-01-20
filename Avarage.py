7fcf122dc2c7ff01e346ed5eac87e904caf9b3f5
Maawa2015 <mzaman008@gmail.com>
2026-01-20 16:30:57 +0600

    Add files via upload

---
+++ Avarage.py
@@ -0,0 +1,9 @@
+a= int(input("enter the first number: "))
+b= int(input("enter the second number: "))
+c= int(input("enter the third number: "))
+d=int(input("enter the fourth number: ")) 
+d=int(input("enter the fifth number:"))
+
+total=a+b+c+d+e 
+avg= total/5
+print("Avarage",avg)
