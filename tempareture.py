6c3f546e2c29a580c2ee803b57613486432c8819
Maawa2015 <mzaman008@gmail.com>
2026-01-29 14:46:59 +0600

    Add files via upload

---
+++ tempareture.py
@@ -0,0 +1,9 @@
+summer_temp = 36
+winter_temp = 19
+if summer_temp >= 35:
+    print("Summer: Hot")
+else:
+    if summer_temp >= 25:
+        print("Summer: Warm")
+    else:
+        print("Summer: Cold")
